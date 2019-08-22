b3679a2b5711601ebc250ff8d40cf3aa9d3a6f81
Roman <vazhnik.roma@mail.ru>
2019-08-22 12:01:18 +0300

    Initial Commit

---
+++ simpsonsApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simpsonsApi
+//
+//  Created by Роман Важник on 22/08/2019.
+//  Copyright © 2019 Роман Важник. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
